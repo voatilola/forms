5f0ebc81024a04264c2b1fc8e58a8ee377619e8c
voatilola <voatilola@gmail.com>
2019-06-14 10:12:48 +0100

    New update

--- script.js
+++ script.js
@@ -6,28 +6,32 @@ function validateSignUp() {
     var pwd2 = document.getElementById('pwd2');
     var email = document.getElementById('email');
     var terms = document.getElementById('terms');
-    if(Number(age.value) === 0 || Number(age.value) < 0) {
-        alert("Age must be greater than zero");
-        return false;
-    } else if(fname.value === "") {
+    var dropdown = document.getElementById('age_group');
+    if(fname.value === "") {
         alert("First name is required");
         return false;
     } else if(lname.value === "") {
         alert("Last name is required");
         return false;
+    } else if(Number(age.value) === 0 || Number(age.value) < 0) {
+        alert("Age must be greater than zero");
+        return false;
     } else if( email.value === "" && !email.value.includes("@")) {
         alert("Invalid email format");
-        return false;
-    } else if(!terms.value){
-        alert("Please agree to term and condition");
         return false;
     } else if( pwd1.value === "" && pwd1.value.length < 6) {
         alert("Weak password");
         return false;
     } else if( pwd2.value  !== pwd1.value) {
         alert("Wrong password");
+        return false;
+    } else if(dropdown.selectedIndex==0){
+        alert("Please answer secret question");
+        return false;
+    } else if(!terms.value){
+        alert("Please agree to term and condition");
         return false; }
-         else {
+             else {
             return true;
     }
 }
@@ -48,4 +52,3 @@ function validateSignIn() {
     }
         
 }
-
